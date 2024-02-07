c151d4558f9dc72a38da5896a42cc3d88d7f3681
Evgeny Dibrovsky <105384282+EvgenyDibrovsky@users.noreply.github.com>
2024-02-07 10:29:14 +0100

    Updates

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import initSwiper from './modules/swiper';
 import { initAnimateElements } from './modules/animationElements';
 import backToTop from './modules/backToTop';
 import contactFormDhow from './modules/contactFormShow';
+import privacypolicy from './modules/privacy-policy';
 import '../main.css';
 require('fslightbox');
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
   initAnimateElements();
   backToTop();
   contactFormDhow();
+  privacypolicy();
 });
 
 if (import.meta.webpackHot) {
