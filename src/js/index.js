537dca3eb39082ce117df381a2b852e84defbd2b
Alexander Piniuhin <85253534+Piniuhin@users.noreply.github.com>
2024-06-03 15:03:25 +0200

    add

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,6 @@
-import mobileMenu from './modules/mobileMenu';
-import initSwiper from './modules/swiper';
-import { initAnimateElements } from './modules/animationElements';
-import backToTop from './modules/backToTop';
-import contactFormDhow from './modules/contactFormShow';
-import privacypolicy from './modules/privacy-policy';
-import cleanForm from './modules/cleanform';
 import '../main.css';
-require('fslightbox');
-document.addEventListener('DOMContentLoaded', () => {
-  mobileMenu();
-  initSwiper();
-  initAnimateElements();
-  backToTop();
-  contactFormDhow();
-  privacypolicy();
-  cleanForm();
-});
+
+document.addEventListener('DOMContentLoaded', () => {});
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept();
